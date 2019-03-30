8e8b1fb5ca39bbcea46db58d8f3744edc22ddc1c
Vera Svistun <vera.svistun05@gmail.com>
2019-03-30 11:38:27 +0300

    Внесла правки

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    function clock() {
+        var date = new Date(),
+            day = date.getDate(),
+            month = date.getMonth(),
+            monthArr = ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+            year = date.getFullYear(),
+            hour = date.getHours(),
+            min = date.getMinutes();
+
+    }
+
+}
