93efd85fa1df636080f7f09eafe3021523914c70
Vera Svistun <vera.svistun05@gmail.com>
2019-04-03 12:06:10 +0300

    Внесла правки

--- js/script.js
+++ js/script.js
@@ -1,28 +1,33 @@
-window.onload = function () {
-    function clock() {
-        var date = new Date(),
-            day = date.getDate(),
-            month = date.getMonth(),
-            monthArr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-            year = date.getFullYear(),
-            hour = date.getHours(),
-            min = date.getMinutes();
+$(document).ready(function () {
 
-        if (day < 10) day = '0' + day;
-        if (hour < 10) hour = "0" + hour;
-        if (min < 10) min = "0" + min;
+    window.onload = function () {
+        function clock() {
+            var date = new Date(),
+                day = date.getDate(),
+                month = date.getMonth(),
+                monthArr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                year = date.getFullYear(),
+                hour = date.getHours(),
+                min = date.getMinutes();
 
-        document.getElementById("date").innerHTML = day + " " + monthArr[month];
-        document.getElementById("time").innerHTML = hour + ":" + min;
-        document.getElementById("year").innerHTML = year;
+            if (day < 10) day = '0' + day;
+            if (hour < 10) hour = "0" + hour;
+            if (min < 10) min = "0" + min;
+
+            document.getElementById("date").innerHTML = day + " " + monthArr[month];
+            document.getElementById("time").innerHTML = hour + ":" + min;
+            document.getElementById("year").innerHTML = year;
+        }
+
+        var timer;
+
+        function clockStart() {
+            timer = setInterval(clock, 1000);
+            clock();
+        }
+
+        clockStart();
+
     }
 
-    var timer;
-
-    function clockStart() {
-        timer = setInterval(clock, 60000);
-        clock();
-    }
-    clockStart();
-
-}
+})
