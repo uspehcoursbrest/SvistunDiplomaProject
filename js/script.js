ee8e35ac00c41196d709319d80434cf3b43bd8d5
Vera Svistun <vera.svistun05@gmail.com>
2019-04-06 10:23:58 +0300

    Внесла правки

--- js/script.js
+++ js/script.js
@@ -1,56 +1,49 @@
 $(document).ready(function () {
 
-    window.onload = function () {
-        function clock() {
-            var date = new Date(),
-                day = date.getDate(),
-                month = date.getMonth(),
-                monthArr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-                year = date.getFullYear(),
-                hour = date.getHours(),
-                min = date.getMinutes();
+    function clock() {
+        var date = new Date(),
+            day = date.getDate(),
+            month = date.getMonth(),
+            monthArr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            year = date.getFullYear(),
+            hour = date.getHours(),
+            min = date.getMinutes();
 
-            if (day < 10) day = '0' + day;
-            if (hour < 10) hour = "0" + hour;
-            if (min < 10) min = "0" + min;
+        if (day < 10) day = '0' + day;
+        if (hour < 10) hour = "0" + hour;
+        if (min < 10) min = "0" + min;
 
-            document.getElementById("date").innerHTML = day + " " + monthArr[month];
-            document.getElementById("time").innerHTML = hour + ":" + min;
-            document.getElementById("year").innerHTML = year;
-        }
-
-        var timer;
-
-        function clockStart() {
-            timer = setInterval(clock, 1000);
-            clock();
-        }
-
-        clockStart();
-
-        $('select').selectric();
-
-        var accord = $('.accordion');
-
-        accord.find('.row:not(.active)').siblings('div').slideUp();
-
-        accord.find('.row').on('click', function () {
-
-            $(this).siblings('div').stop().slideToggle(500);
-            if ($(this).hasClass('active')) {
-                $(this).removeClass('active');
-            } else {
-                $(this).addClass('active');
-            }
-
-
-        })
-
-
+        document.getElementById("date").innerHTML = day + " " + monthArr[month];
+        document.getElementById("time").innerHTML = hour + ":" + min;
+        document.getElementById("year").innerHTML = year;
     }
 
+    var timer;
+
+    function clockStart() {
+        timer = setInterval(clock, 1000);
+        clock();
+    }
+
+    clockStart();
+
+    $('select').selectric();
+
+    var accord = $('.accordion');
+
+    accord.find('.row:not(.active)').siblings('div').slideUp();
+
+    accord.find('.row').on('click', function () {
+
+        $(this).siblings('div').stop().slideToggle(500);
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        } else {
+            $(this).addClass('active');
+        }
 
 
+    })
 
 
 })
